4f1c68ae6afe0a90d036a2026f78e53b9e9aa69b
Mel Abgrall <mae.a@outlook.com>
2018-04-26 22:20:18 +0200

    modals

--- pages/en/navbar.js
+++ pages/en/navbar.js
@@ -23,7 +23,7 @@ function init() {
 
 
   navstring += '<nav class="navbar navbar-expand-lg py-1">';
-  navstring += '  <a class="navbar-brand" href="/pages/fr/home.html">Mael Abgrall</a>';
+  navstring += '  <a class="navbar-brand" href="/pages/en/home.html">Mael Abgrall</a>';
   navstring += '  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">';
   navstring += '    <i class="fas fa-bars"></i>';
   navstring += '  </button>';
@@ -33,19 +33,19 @@ function init() {
 
   navstring += '      <li class="nav-item py-0">';
   if (page.indexOf("ccueil") != -1) {
-    navstring += '      <a class="nav-link active" href="/pages/fr/home.html">';
+    navstring += '      <a class="nav-link active" href="/pages/en/home.html">';
   } else {
-    navstring += '      <a class="nav-link" href="/pages/fr/home.html">';
+    navstring += '      <a class="nav-link" href="/pages/en/home.html">';
   }
-  navstring += '          <i class="fas fa-home"></i> Accueil</a>';
+  navstring += '          <i class="fas fa-home"></i> Home</a>';
   navstring += '      </li>';
 
 
   navstring += '      <li class="nav-item py-0">';
   if (page.indexOf("ducation") != -1) {
-    navstring += '      <a class="nav-link active" href="/pages/fr/education.html">';
+    navstring += '      <a class="nav-link active" href="/pages/en/education.html">';
   } else {
-    navstring += '      <a class="nav-link" href="/pages/fr/education.html">';
+    navstring += '      <a class="nav-link" href="/pages/en/education.html">';
   }
   navstring += '          <i class="fas fa-graduation-cap"></i> Education</a>';
   navstring += '      </li>';
@@ -53,39 +53,39 @@ function init() {
 
   navstring += '      <li class="nav-item">';
   if (page.indexOf("xperience") != -1) {
-    navstring += '      <a class="nav-link active" href="/pages/fr/experience.html">';
+    navstring += '      <a class="nav-link active" href="/pages/en/experience.html">';
   } else {
-    navstring += '      <a class="nav-link" href="/pages/fr/experience.html">';
+    navstring += '      <a class="nav-link" href="/pages/en/experience.html">';
   }
-  navstring += '          <i class="fas fa-suitcase"></i> Expérience</a>';
+  navstring += '          <i class="fas fa-suitcase"></i> Experience</a>';
   navstring += '      </li>';
 
 
   navstring += '      <li class="nav-item">';
   if (page.indexOf("rojet") != -1) {
-    navstring += '      <a class="nav-link active" href="/pages/fr/projet.html">';
+    navstring += '      <a class="nav-link active" href="/pages/en/projet.html">';
   } else {
-    navstring += '      <a class="nav-link" href="/pages/fr/projet.html">';
+    navstring += '      <a class="nav-link" href="/pages/en/projet.html">';
   }
-  navstring += '          <i class="fas fa-lightbulb"></i> Projets</a>';
+  navstring += '          <i class="fas fa-lightbulb"></i> Projects</a>';
   navstring += '      </li>';
 
 
   navstring += '      <li class="nav-item">';
   if (page.indexOf("kill") != -1) {
-    navstring += '      <a class="nav-link active" href="/pages/fr/skill.html">';
+    navstring += '      <a class="nav-link active" href="/pages/en/skill.html">';
   } else {
-    navstring += '      <a class="nav-link" href="/pages/fr/skill.html">';
+    navstring += '      <a class="nav-link" href="/pages/en/skill.html">';
   }
-  navstring += '          <i class="fas fa-wrench"></i> Compétences</a>';
+  navstring += '          <i class="fas fa-wrench"></i> Skills</a>';
   navstring += '      </li>';
 
 
   navstring += '      <li class="nav-item">';
   if (page.indexOf("obby") != -1) {
-    navstring += '      <a class="nav-link active" href="/pages/fr/hobby.html">';
+    navstring += '      <a class="nav-link active" href="/pages/en/hobby.html">';
   } else {
-    navstring += '      <a class="nav-link" href="/pages/fr/hobby.html">';
+    navstring += '      <a class="nav-link" href="/pages/en/hobby.html">';
   }
   navstring += '          <i class="fas fa-map-signs"></i> Hobbies</a>';
   navstring += '      </li>';
@@ -93,9 +93,9 @@ function init() {
 
   navstring += '      <li class="nav-item">';
   if (page.indexOf("ontact") != -1) {
-    navstring += '      <a class="nav-link active" href="/pages/fr/contact.html">';
+    navstring += '      <a class="nav-link active" href="/pages/en/contact.html">';
   } else {
-    navstring += '      <a class="nav-link" href="/pages/fr/contact.html">';
+    navstring += '      <a class="nav-link" href="/pages/en/contact.html">';
   }
   navstring += '          <i class="fas fa-address-card"></i> Contact</a>';
   navstring += '      </li>';
@@ -107,11 +107,11 @@ function init() {
   navstring += '    <ul class="navbar-nav ml-auto">';
   navstring += '      <li class="nav-item dropdown">';
   navstring += '        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">';
-  navstring += '          <i class="fas fa-flag"></i> Langue';
+  navstring += '          <i class="fas fa-flag"></i> Language';
   navstring += '        </a>';
   navstring += '        <div class="dropdown-menu">';
-  navstring += '          <a class="dropdown-item active" href="/pages/fr/home.html">Français</a>';
-  navstring += '          <a class="dropdown-item" href="/pages/en/home.html">Anglais</a>';
+  navstring += '          <a class="dropdown-item" href="/pages/fr/home.html">French</a>';
+  navstring += '          <a class="dropdown-item active" href="/pages/en/home.html">English</a>';
   navstring += '        </div>';
   navstring += '      </li>';
 
